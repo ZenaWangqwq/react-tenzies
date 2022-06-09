d17886b04da0ed20377d95235a6cac1674364aa3
“your_name” <zena.Wang@kcl.ac.uk>
2022-06-09 23:21:01 +0100

    basic prototype

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Die from "./components/Die"
+
+export default function App() {
+    return (
+        <main>
+            <div className="container">
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+                <div className="die"><Die /></div>
+            </div>
+        </main>
+    )
+}
---
+++ src/components/Die.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Die() {
+    return (
+        <div className="die">
+            1
+        </div>
+    )
+}
