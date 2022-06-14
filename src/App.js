423d064263a12cb5a8522ccc2d1e251021a75de9
“your_name” <zena.Wang@kcl.ac.uk>
2022-06-14 17:21:25 +0100

    add die and roll function

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
+import { nanoid } from "nanoid"
 import React from "react"
 import Die from "./components/Die"
 
+
+
 export default function App() {
+
+    const [diceArray, setDiceArray] = React.useState(allNewDice())
+
+    function generateNewDie() {
+        return {
+            id: nanoid(),
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false
+        }
+    }
+
+    function allNewDice() {
+        const diceArray = []
+        for (let i = 0; i < 10; i++){
+            diceArray.push(generateNewDie())
+        }
+        return diceArray
+    }
+
+    function roll() {
+        setDiceArray(prevArray => prevArray.map(die => {
+            return die.isHeld ? 
+                die :
+                generateNewDie()
+        }))
+    }
+
+    function holdDice(id) {
+        setDiceArray(prevArray => prevArray.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+
+    const diceElements = diceArray.map(die => 
+        <Die 
+            key={die.id} 
+            id={die.id}
+            value={die.value}
+            isHeld={die.isHeld}
+            holdDice={() => holdDice(die.id)}
+        />
+    )
+
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. 
+            Click each die to freeze it at its current value between rolls.</p>
             <div className="container">
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
-                <div className="die"><Die /></div>
+                {diceElements}
             </div>
+            <button className="roll-dice" onClick={roll}>Roll</button>
         </main>
     )
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -1,9 +1,13 @@
 import React from "react"
 
-export default function Die() {
+export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
     return (
-        <div className="die">
-            1
+        <div style={styles} className="die" onClick={props.holdDice}>
+            {props.value}
         </div>
     )
 }
