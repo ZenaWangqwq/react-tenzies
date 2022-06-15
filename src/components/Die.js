2ecc4b8984dfc3984d5da38db1abe2dc81946dc8
“your_name” <zena.Wang@kcl.ac.uk>
2022-06-15 16:26:35 +0100

    arrange dots by grid-template-area

--- src/components/Die.js
+++ src/components/Die.js
@@ -5,50 +5,10 @@ export default function Die(props) {
         backgroundColor: props.isHeld ? "#59E391" : "white"
     }
 
-    const dotsStyles =  {}
-
     const dotElements = []
 
-    function determineNumberOfDots() {
-        if (props.value === 3) {
-            return "three-dots"
-        }
-        else if (props.value === 5) {
-            return "five-dots"
-        }
-        else return "dots"
-    }
-
     for (let i = 0; i < props.value; i++){
-        dotElements.push(<span style={dotsStyles} className= {determineNumberOfDots()} />)
-    }
-
-    switch(props.value) {
-        case 1:
-            styles.display = "flex";
-            styles.justifyContent = "center";
-            styles.alignItems = "center";
-            break;
-        case 2:
-            styles.display = "flex";
-            styles.justifyContent = "space-around";
-            styles.alignItems = "center";
-            break;
-        case 3:
-            styles.display = "flex";
-            styles.justifyContent = "space-between";
-            styles.padding = "5px";
-            break;
-        case 4:
-            styles.display = "grid";
-            styles.gridTemplate = "1fr 1fr/ 1fr 1fr";
-            styles.placeItems = "center";
-            break;
-        case 6:
-            styles.display = "grid";
-            styles.gridTemplate = "1fr 1fr 1fr/ 1fr 1fr";
-            styles.placeItems = "center";
-            break;
+        dotElements.push(<span className="dots" />)
     }
 
     return (
