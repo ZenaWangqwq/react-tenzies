926cdf2b7d93a7d6d5f924224c1ac4636ea94166
“your_name” <zena.Wang@kcl.ac.uk>
2022-06-16 12:27:21 +0100

    fix error for store best time and timer

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,78 +3,77 @@ import React from "react"
 export default function Header(props) {
     const [minutes, setMinutes] = React.useState(0);
     const [seconds, setSeconds] = React.useState(0);
-    const [bestTime, setBestTime] = React.useState(localStorage.getItem("bestTime")||{minutes: 99,seconds: 99})
+    const [bestTime, setBestTime] = React.useState(localStorage.getItem("bestTime")||"Not Recorded Yet")
 
-    React.useEffect(
-            () => {
-                if (props.tenzies) {
-                    getBestTime()
-                    return;
-                }
+    React.useEffect(() => {
+            if (props.tenzies === true) {
+                getBestTime()
+                return;
+            }
 
-                const timer = () => {
-                    setSeconds(seconds + 1);
-                }
+            const timer = () => {
+                setSeconds(seconds + 1);
+            }
 
-                if (seconds%60 >= 59) {
-                    setMinutes(minutes => minutes+1)
-                }
+            if (seconds%60 === 0 && seconds !== 0) {
+                setMinutes(minutes => minutes+1)
+            }
 
-                const id = setInterval(timer, 1000);
-                return () => clearInterval(id);
-            },
-            [seconds]
-        );
+            const id = setInterval(timer, 1000);
+            return () => clearInterval(id);
+    },[seconds, props.tenzies]);
 
-    function showTime() {
-        let showMinutes = minutes
-        let showSeconds = seconds
-        if (minutes < 10){
-            showMinutes = "0" + minutes
+    React.useEffect(() => {
+        if (props.tenzies === true) {
+            getBestTime()
+        }else{
+            setSeconds(0)
+            setMinutes(0)
         }
-        if (seconds < 10){
-            showSeconds = "0" + seconds
-        }
-        return showMinutes + " : " + showSeconds
-    }
-
-    function getBestTime() {
-
-        setBestTime({
-            minutes:minutes,
-            seconds:seconds
-        })
-
-        // setBestTime({
-        //     minutes:minutes,
-        //     seconds:seconds
-        // })
-        // if (bestTime.minutes > minutes) {
-        //     setBestTime({
-        //         minutes:minutes,
-        //         seconds:seconds
-        //     })
-        // }
-        // else if (bestTime.minutes = minutes) {
-        //     if (bestTime.seconds >= seconds) {
-        //         setBestTime({
-        //             minutes:minutes,
-        //             seconds:seconds
-        //         })
-        //     }
-        // }
-    }
-
-    console.log(bestTime)
+    }, [props.tenzies])
 
     React.useEffect(() => {
         localStorage.setItem("bestTime", bestTime)
     },[bestTime])
 
+    function showTime(min, sec) {
+        let showMinutes = min
+        let showSeconds = sec%60
+        if (minutes < 10){
+            showMinutes = "0" + showMinutes
+        }
+        if ((seconds%60) < 10){
+            showSeconds = "0" + showSeconds
+        }
+        return showMinutes + " : " + showSeconds
+    }
+    
+    function getBestTime() {
+        if(bestTime==="Not Recorded Yet"){
+            setBestTime(JSON.stringify([minutes,seconds%60]))
+            return
+        }
+
+        let bestTimeArray = JSON.parse(bestTime)
+        
+        const bestTimeMinutes = bestTimeArray[0]
+        const bestTimeSeconds = bestTimeArray[1]
+
+        if (bestTimeMinutes > minutes) {
+            setBestTime(JSON.stringify([minutes,seconds%60]))
+            return
+        }
+        else if (bestTimeMinutes === minutes) {
+            if (bestTimeSeconds >= seconds) {
+                setBestTime(JSON.stringify([minutes,seconds%60]))
+            }
+        }
+    }
+
     return (
         <header>
             <h4>Rolls: <font color="blue" size="3">{props.rollCount}</font></h4>
-            <h4>Time: <font color="red" size="3">{showTime()}</font></h4>
+            <h4>Time: <font color="red" size="3">{showTime(minutes, seconds)}</font></h4>
             <h4>Best Time: <font color="green" size="3">{bestTime}</font></h4>
         </header>
     )
